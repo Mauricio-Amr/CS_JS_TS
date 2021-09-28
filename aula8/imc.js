af64c8efdbab52deb05926f89279a58200ece4e4
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2021-09-28 13:01:08 -0300

    retornando ao javascript

--- aula8/imc.js
+++ aula8/imc.js
@@ -7,8 +7,8 @@ const sobrenome = 'Amaral'
 const idade = 36
 const peso = 78
 const alturaM = 1.80
-let imc = peso / (Math.pow(altura, 2))
-let ano_nas = 2021 -36;
+let imc = peso / (Math.pow(alturaM, 2))
+let ano_nas = 2021 - idade;
 
 console.log(`${nome} ${sobrenome} tem ${idade} anos , pesa ${peso}`)
 console.log(`tem ${alturaM} de altura e seu IMC ${imc.toFixed(2)}`)
